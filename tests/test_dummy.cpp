4f36e9c8ba0cab229c8ff41aa05ccf97e8d7400f
Richard Magnor Stenbro <stenbror@hotmail.com>
2025-07-04 19:28:29 +0200

    init

---
+++ tests/test_dummy.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Richard Magnor Stenbro on 04/07/2025.
+//
+#define BOOST_TEST_MODULE NativePythonCore Tests
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(sanity_check)
+{
+    BOOST_TEST(true);
+}
