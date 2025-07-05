2ef1f6f9dd2eacd84b50f710edb10d5ab516da54
Richard Magnor Stenbro <stenbror@hotmail.com>
2025-07-05 19:31:53 +0200

    Lexer public methods is in place.

--- src/parser/Lexer.cpp
+++ src/parser/Lexer.cpp
@@ -4,6 +4,57 @@
 
 #include "Lexer.h"
 
+#include <utility>
+
 
 using NativePythonCore::Parser::Lexer;
 
+// Public constructor and methods //////////////////////////////////////////////////////////////////////////////////////
+
+Lexer::Lexer(std::basic_string<char8_t> source_code, const unsigned long tab_size) {
+    m_source_code = std::move(source_code);
+    m_tab_size = tab_size;
+    m_symbol_index = 0;
+    m_line = 1;
+    m_column = 0;
+    m_start_index = 0;
+    m_end_index = 0;
+    m_prefix = std::vector<std::shared_ptr<Trivia>>();
+    m_trailer = std::vector<std::shared_ptr<Trivia>>();
+
+    CollectSymbols();
+}
+
+std::shared_ptr<NativePythonCore::Parser::Symbol> Lexer::GetNextSymbol() {
+    return m_symbol_index < m_symbols.size() ?
+        m_symbols[m_symbol_index++]
+        : std::make_shared<Symbol>(SymbolType::kw_eof, m_line, m_column, m_start_index, m_end_index, m_prefix, m_trailer);
+}
+
+std::shared_ptr<NativePythonCore::Parser::Symbol> Lexer::GetNextSymbol(const unsigned long index) {
+    return index <  m_symbols.size() ?
+        m_symbols[index]
+        : std::make_shared<Symbol>(SymbolType::kw_eof, m_line, m_column, m_start_index, m_end_index, m_prefix, m_trailer);
+}
+
+unsigned long Lexer::SymbolIndex() const
+{
+    return m_symbol_index;
+}
+
+void Lexer::Reset() {
+    m_symbol_index = 0;
+    m_line = 1;
+    m_column = 0;
+    m_start_index = 0;
+    m_end_index = 0;
+    m_prefix = std::vector<std::shared_ptr<Trivia>>();
+    m_trailer = std::vector<std::shared_ptr<Trivia>>();
+    m_Parenthesis_stack = std::stack<char8_t>();
+    m_indent_stack = std::stack<unsigned long>();
+}
+
+void Lexer::CollectSymbols()
+{
+
+}
--- src/parser/Lexer.h
+++ src/parser/Lexer.h
@@ -4,12 +4,38 @@
 
 #ifndef LEXER_H
 #define LEXER_H
+#include <stack>
+#include <vector>
+
+#include "Symbol.h"
 
 namespace NativePythonCore::Parser
 {
 
     class Lexer {
+        std::vector<std::shared_ptr<Symbol>> m_symbols;
+        std::basic_string<char8_t> m_source_code;
+        unsigned long m_tab_size;
+        unsigned long m_symbol_index;
+        unsigned long m_line;
+        unsigned long m_column;
+        unsigned long m_start_index;
+        unsigned long m_end_index;
+        std::vector<std::shared_ptr<Trivia>> m_prefix;
+        std::vector<std::shared_ptr<Trivia>> m_trailer;
+        std::stack<char8_t> m_Parenthesis_stack;
+        std::stack<unsigned long> m_indent_stack;
 
+    public:
+        Lexer(std::basic_string<char8_t> source_code, unsigned long tab_size);
+
+        std::shared_ptr<Symbol> GetNextSymbol();
+        std::shared_ptr<Symbol> GetNextSymbol(unsigned long index);
+        [[nodiscard]] unsigned long SymbolIndex() const;
+        void Reset();
+
+    private:
+        void CollectSymbols();
     };
 
 }
