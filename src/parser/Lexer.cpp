1fc0aca1dbe357450e7b38d505754d9137b251d4
Richard Magnor Stenbro <stenbror@hotmail.com>
2025-07-05 23:56:39 +0200

    Implemented and tested star / power.

--- src/parser/Lexer.cpp
+++ src/parser/Lexer.cpp
@@ -110,6 +110,35 @@ void Lexer::CollectSymbols()
                 }
                 break;
             }
+            case '*': {
+                it = next;
+                if (auto [codepoint, next] = DecodeUTF8(it, end); codepoint == '*') {
+                    it = next;
+                    if (auto [codepoint, next] = DecodeUTF8(it, end); codepoint == '=') {
+                        it = next;
+                        m_end_index = it - m_source_code.cbegin();
+                        move_index = 3;
+                        symbol = SymbolType::kw_power_assign;
+                    }
+                    else {
+                        m_end_index = it - m_source_code.cbegin();
+                        move_index = 2;
+                        symbol = SymbolType::kw_power;
+                    }
+                }
+                else if (auto [codepoint, next] = DecodeUTF8(it, end); codepoint == '=') {
+                    it = next;
+                    m_end_index = it - m_source_code.cbegin();
+                    move_index = 2;
+                    symbol = SymbolType::kw_star_assign;
+                }
+                else {
+                    m_end_index = it - m_source_code.cbegin();
+                    move_index = 1;
+                    symbol = SymbolType::kw_star;
+                }
+                break;
+            }
             default:
                 break;
         }
--- tests/parser/test_lexical_analyzer.cpp
+++ tests/parser/test_lexical_analyzer.cpp
@@ -89,4 +89,48 @@ BOOST_AUTO_TEST_CASE(test_operator_minus) {
     BOOST_TEST(symbol->GetEndIndex() == 1);
 }
 
+BOOST_AUTO_TEST_CASE(test_operator_power_assign) {
+    const auto lex = new Lexer(u8"**=", 8);
+    const auto symbol = lex->GetNextSymbol();
+
+    BOOST_TEST(symbol->GetSymbolKind() == SymbolType::kw_power_assign);
+    BOOST_TEST(symbol->GetLine() == 1);
+    BOOST_TEST(symbol->GetColumn() == 1);
+    BOOST_TEST(symbol->GetStartIndex() == 0);
+    BOOST_TEST(symbol->GetEndIndex() == 3);
+}
+
+BOOST_AUTO_TEST_CASE(test_operator_power) {
+    const auto lex = new Lexer(u8"**", 8);
+    const auto symbol = lex->GetNextSymbol();
+
+    BOOST_TEST(symbol->GetSymbolKind() == SymbolType::kw_power);
+    BOOST_TEST(symbol->GetLine() == 1);
+    BOOST_TEST(symbol->GetColumn() == 1);
+    BOOST_TEST(symbol->GetStartIndex() == 0);
+    BOOST_TEST(symbol->GetEndIndex() == 2);
+}
+
+BOOST_AUTO_TEST_CASE(test_operator_star_assign) {
+    const auto lex = new Lexer(u8"*=", 8);
+    const auto symbol = lex->GetNextSymbol();
+
+    BOOST_TEST(symbol->GetSymbolKind() == SymbolType::kw_star_assign);
+    BOOST_TEST(symbol->GetLine() == 1);
+    BOOST_TEST(symbol->GetColumn() == 1);
+    BOOST_TEST(symbol->GetStartIndex() == 0);
+    BOOST_TEST(symbol->GetEndIndex() == 2);
+}
+
+BOOST_AUTO_TEST_CASE(test_operator_star) {
+    const auto lex = new Lexer(u8"*", 8);
+    const auto symbol = lex->GetNextSymbol();
+
+    BOOST_TEST(symbol->GetSymbolKind() == SymbolType::kw_star);
+    BOOST_TEST(symbol->GetLine() == 1);
+    BOOST_TEST(symbol->GetColumn() == 1);
+    BOOST_TEST(symbol->GetStartIndex() == 0);
+    BOOST_TEST(symbol->GetEndIndex() == 1);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
